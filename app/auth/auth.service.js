218ea99bd4180c6c1350440c7955fc4ad19addc3
Matthew Tse <mtse9779@gmail.com>
2016-02-24 22:24:19 -0500

    added auth function and login function

--- app/auth/auth.service.js
+++ app/auth/auth.service.js
@@ -8,4 +8,16 @@ angular.module('angularfireSlackApp')
 angular.module('angularfireSlackApp')
   .controller('AuthCtrl', function(Auth, $state){
     var authCtrl = this;
-  })
+
+    authCtrl.user = {
+      email:'',
+      password:''
+    };
+  });
+authCtrl.login = function(){
+  Auth.$authWithPassword(authCtrl.user).then(function(auth){
+    $state.go('home');
+  }, function (error){
+    authCtrl.error = error;
+  });
+}
