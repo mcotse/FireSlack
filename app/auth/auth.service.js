8e988efc58db42631bd8b6c2e4214dc070b29934
Matthew Tse <mtse9779@gmail.com>
2016-02-29 22:14:51 -0500

    Revert "added user factory and controller"
    This reverts commit 2e68f809d8a75eff1840f11797fc9e82786d755c.

--- app/auth/auth.service.js
+++ app/auth/auth.service.js
@@ -4,19 +4,4 @@ angular.module('angularfireSlackApp')
     var auth = $firebaseAuth(ref);
 
     return auth;
-  })
-  .factory('Users',funciton($firebaseArray, $firebaseObject, FirebaseUrl){
-    var usersRef = new Firebase(FirebaseUrl+'users');
-    var users = $firebaseArray(usersRef)
-    var Users = {
-      getProfile: function(uid){
-        return $firebaseObject(usersRef.child(uid));
-      },
-      getDisplayName: function(uid){
-        return users.$getRecord(uid).displayName;
-      },
-      all:users
-    };
-
-    return Users;
   });
