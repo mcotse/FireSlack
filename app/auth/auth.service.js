86a15e16aec86056fb3e9c900839a9303efd7a31
Matthew Tse <mtse9779@gmail.com>
2016-02-29 20:59:40 -0500

    added parameters to factory

--- app/auth/auth.service.js
+++ app/auth/auth.service.js
@@ -1,5 +1,5 @@
 angular.module('angularfireSlackApp')
-  .factory('Auth',function(){
+  .factory('Auth',function($firebaseAuth, FirebaseUrl){
     var ref = new Firebase(FirebaseUrl);
     var auth = $firebaseAuth(ref);
 
